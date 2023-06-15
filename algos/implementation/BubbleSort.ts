ba49c50db72cac3dc55847dc12afd1cfa7d5cda9
Rob Schroder <rob.schroder@student.unimelb.edu.au>
2023-06-15 10:16:31 +1000

    BubbleSort Implemented

--- algos/implementation/BubbleSort.ts
+++ algos/implementation/BubbleSort.ts
@@ -5,17 +5,19 @@ export default class BubbleSort implements IAlgorithm {
     // Array of data to sort
     private data: Array<number>;
     private isFinished: boolean = false;
+    private currentIndex: number;
 
     constructor(data: Array<number>){
         // Convert number to custom data type
-        
-
         this.data = data;
+        // Insertion sort starts by comparing 1st element to the 0th
+        this.currentIndex = 1;
     }
 
     // Modify the data one step at a time
     public step(): Array<number> {
         // Sort 1 step in algo
+        
 
         // Update isFinished
 
--- components/buttons/runAlgo/RunAlgo.tsx
+++ components/buttons/runAlgo/RunAlgo.tsx
@@ -20,46 +20,6 @@ export default function RunAlgo() {
       }
     }
 
-    async function bubbleSort(changeArray: number[]) {
-      for (let i = 0; i < changeArray.length; i++) {
-        for (let j = 0; j < changeArray.length - 1; j++) {
-          const array = [...changeArray];
-  
-          if (changeArray[j] > changeArray[j + 1]) {
-            let temp = changeArray[j];
-            changeArray[j] = changeArray[j + 1];
-            changeArray[j + 1] = temp;
-  
-            array[j] = changeArray[j];
-            array[j + 1] = changeArray[j + 1];
-  
-            await delay(100); // Delay between each batch (adjust the duration as needed)
-  
-            dispatch(updateArray(array));
-          }
-        }
-      }
-    }
-
-    async function linearSort(changeArray: number[]) {
-        const array = [...changeArray];
-      
-        for (let i = 0; i < array.length - 1; i++) {
-          let minIndex = i;
-      
-          for (let j = i + 1; j < array.length; j++) {
-            if (array[j] < array[minIndex]) {
-              minIndex = j;
-            }
-          }
-      
-          if (minIndex !== i) {
-            // Swap elements
-            [array[i], array[minIndex]] = [array[minIndex], array[i]];
-          }
-        }
-      }
-
     function delay(duration: number): Promise<void> {
       return new Promise((resolve) => {
         setTimeout(resolve, duration);
