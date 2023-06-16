1bf3ba3ef35f59b0c0c2f5d09adf91259c9f4d15
loganr-j <logan.rogers.jenkins@gmail.com>
2023-06-16 16:22:07 +1200

    added icon and reformatted display

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,22 +1,24 @@
 import React from "react";
-import { Grid, Icon, Link } from "@mui/material";
+import { Grid,  Link } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
-
+import AlgositeIcon from "@/public/AlgositeIcon.png";
 
 export const Header = () => {
-  return (
-    <Grid className="header" >
-      <h1>Algosite</h1>
-      <Link
-        href="https://github.com/squizzles/display-sorting-algorithms"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="header__icon"
-      >
-        <GitHubIcon />
-      </Link>
-    </Grid>
-  );
+	return (
+		<Grid className="header">
+        <Grid className="title">
+				  <h1>Algosite</h1>
+          <img src={AlgositeIcon.src} alt="Algosite Icon" className="header__icon" />
+        </Grid>
+				<Link
+					href="https://github.com/squizzles/display-sorting-algorithms"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="header__icon"
+				>
+					<GitHubIcon />
+				</Link>
+			
+		</Grid>
+	);
 };
-
-
