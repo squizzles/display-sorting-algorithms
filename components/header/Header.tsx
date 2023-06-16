b25302e79ecb23fd736c16e1b30a91ecbf764628
loganr-j <logan.rogers.jenkins@gmail.com>
2023-06-16 16:27:41 +1200

    altered image

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Grid,  Link } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import AlgositeIcon from "@/public/AlgositeIcon.png";
+import Image from "next/image";
 
 export const Header = () => {
 	return (
 		<Grid className="header">
         <Grid className="title">
 				  <h1>Algosite</h1>
-          <img src={AlgositeIcon.src} alt="Algosite Icon" className="header__icon" />
+          <Image src={AlgositeIcon.src} alt="Algosite Icon" className="header__icon" width={100} height={100}  />
         </Grid>
 				<Link
 					href="https://github.com/squizzles/display-sorting-algorithms"
